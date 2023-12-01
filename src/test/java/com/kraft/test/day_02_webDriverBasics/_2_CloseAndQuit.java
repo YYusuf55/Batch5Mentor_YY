9634ab1450288ae4fb03fdc8f19792eb2a853ace
yusufyıldız <ysff94@gmail.com>
2023-12-01 20:17:09 +0300

    first commit

---
+++ src/test/java/com/kraft/test/day_02_webDriverBasics/_2_CloseAndQuit.java
@@ -0,0 +1,25 @@
+package com.kraft.test.day_02_webDriverBasics;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _2_CloseAndQuit {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("https://www.facebook.com/");
+        Thread.sleep(2000);
+        driver.close();
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver1=new ChromeDriver();
+
+        driver1.get("https://testrelic.com/");
+        driver1.quit();
+
+        driver=new ChromeDriver();
+        driver.navigate().to("https://www.google.com");
+    }
+}
